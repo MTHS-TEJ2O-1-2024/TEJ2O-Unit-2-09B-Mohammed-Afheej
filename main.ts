7212b7e2ea6f7a59de56754768057f24ea8c7b64
Mohammed-Afheej <mohammed.afheej@mths.ca>
2024-10-10 11:15:50 -0400

    edited the comment

--- main.ts
+++ main.ts
@@ -1,8 +1,54 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mohammed 
+ * Created on: Oct
+ * This program plays rock, paper, Scissors
 */
 
-basic.showString('Hello, World!')
+let randomNumber: number = 0
+let totalPoint: number = 0
+
+totalPoint = 0
+randomNumber = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+
+    // if the number is 0
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+        pause(5000)
+        basic.clearScreen()
+        basic.showIcon(IconNames.Happy)
+    }
+
+    // if the number is 1
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.Square)
+        pause(5000)
+        basic.clearScreen()
+        basic.showIcon(IconNames.Happy)
+    }
+
+    // if the number is 2
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+        pause(5000)
+        basic.clearScreen()
+        basic.showIcon(IconNames.Happy)
+    }
+})
+
+input.onButtonPressed(Button.A, function () {
+    totalPoint = totalPoint + 1
+    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Score: " + totalPoint)
+    basic.showIcon(IconNames.Happy)
+})
